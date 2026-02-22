29f5677388946ba5e307d379947aee1954148bfe
João Pedro <joao.custodio@estudante.iftm.edu.br>
2026-02-22 16:34:05 -0300

    geral changes

--- test_v2/evaluator/test_evaluator_v2_suite_all.c
+++
@@ -1,7 +0,0 @@
-#include "test_v2_suite.h"
-
-void run_evaluator_golden_tests(int *passed, int *failed);
-
-void run_evaluator_v2_tests(int *passed, int *failed) {
-    run_evaluator_golden_tests(passed, failed);
-}
